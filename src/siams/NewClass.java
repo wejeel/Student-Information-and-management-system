efc91fe5c4c01391450cb80367c0d2732a77587d
Weje Praise <wejepraise@yahoo.com>
2019-08-21 09:34:51 +0100

    initialize

---
+++ src/siams/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package siams;
+
+/**
+ *
+ * @author WEJE
+ */
+public class NewClass {
+   
+}
